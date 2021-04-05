921947e762c0d58fe8d3241be817e1234a23076e
lcy2317 <lcy2317@users.noreply.github.com>
2021-04-05 18:33:50 +0800

    init

---
+++ eureka-server/src/test/java/com/lcy/eureka/server/EurekaServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.lcy.eureka.server;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
